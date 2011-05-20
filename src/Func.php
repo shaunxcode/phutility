f081d7381e1271b843d126296dd34558c1049829
shaun gilchrist <shaunxcode@gmail.com>
2011-05-19 22:41:47 -0600

    keyword arguments, multi method dispatch by type working, numeric next

--- src/Func.php
+++ src/Func.php
@@ -3,6 +3,21 @@
 namespace Phutility;
 
 class Func {
+	const x = 'wildcard';
+	
+	public static function reflect($func) {
+		$r = new \ReflectionObject($func);
+		return $r->getMethod('__invoke');
+	}
+	
+	public static function arity($func) {
+	    return self::reflect($func)->getNumberOfParameters();
+	}
+		
+	public static function getParameters($func) {
+		return self::reflect($func)->getParameters();
+	}
+	
 	public static function rest($func, $transform = false) {		
 		if(!$func instanceof \Closure) {
 			throw new \Exception('1st param must be a closure');
@@ -29,12 +44,86 @@ class Func {
 		};
 	}
 
-	public static function arity($func) {
-	    $r = new \ReflectionObject($func);
-	    return $r->getMethod('__invoke')->getNumberOfParameters();
+	public static function args($func) {
+		return function() use($func) {
+			return $func(func_get_args());
+		};
 	}
 	
 	public static function options($func) {		
 		return Func::rest($func, function($i) { return Appos::create($i); });
+	}
+		
+	public static function keyword($func) {
+		$kwlist = array();
+		foreach(self::getParameters($func) as $arg) {
+			$kwlist[$arg->name] = false;
+		}
+		
+		return self::options(function($args) use($func, $kwlist) {
+			foreach($kwlist as $key => $val) {
+				if(!isset($args[$key])) {
+					throw new \Exception("Expect $key but was not found in " . json_encode(array_keys($args)));
+				}
+				$kwlist[$key] = $args[$key];
+			}
+			return call_user_func_array($func, array_values($kwlist));
+		});
+	}
+	
+	public static function matchType() {
+		$funcs = array();
+		
+		$isMatchBuilder = function($arity, $func, $types) {
+			return Func::args(function($args) use($arity, $func, $types) {
+				if(count($args) !== $arity) {
+					return null;
+				}
+
+				foreach($args as $a => $arg) {
+					if(!is_object($arg)) {
+						var_dump($arg);
+						throw new \Exception("All arguments should be objects");
+					}
+					if(!$arg instanceof $types[$a]) {
+						return $continue;
+					}
+				}
+				
+				return call_user_func_array($func, $args);
+			});
+		};
+		
+		$types = array();		
+		foreach(func_get_args() as $arg) {
+			if(is_string($arg)) { 
+				$types[] = $arg;
+			}
+			
+			if($arg instanceof \Closure) {
+				$funcs[] = $isMatchBuilder(Func::arity($arg), $arg, $types);
+				$types = array();
+			}
+		}
+			
+		if(!empty($type)) {
+			throw new \Exception("Should end w/ closure..");
+		}
+
+		return Func::args(function($args) use($funcs) { 
+			foreach($funcs as $func) {
+				$result = call_user_func_array($func, $args);
+				if(is_null($result)) {
+					continue;
+				} else {
+					return $result;
+				}
+			}
+			throw new \Exception("No match found for args passed");
+		});
+	}
+	
+	public static function match() {
+		return function() {};
 	}
 }
--- tests/FuncTest.php
+++ tests/FuncTest.php
@@ -35,3 +35,40 @@ $func5 = F::options(function($age, $weight, $options) {
 });
 
 test::assert("extra params", $func5(5, 500, 'name', 'peter', 'hair', 'red'), array('age' => 5, 'weight' => 500, 'options' => array('name' => 'peter', 'hair' => 'red')));
+
+$keyword = F::keyword(function($age, $eyeColor) {
+	return array('age' => $age, 'eyeColor' => $eyeColor);
+});
+
+test::assert("keyword args normal", $keyword(age, 25, eyeColor, 'blue'), array('age' => 25, 'eyeColor' => 'blue'));
+test::assert("keyword args reversed", $keyword(eyeColor, 'blue', age, 25), array('age' => 25, 'eyeColor' => 'blue'));
+
+$argsFunc = F::args(function($args) { 
+	return $args;
+});
+
+test::assert("args works", $argsFunc(1, 2, 3), array(1, 2, 3));
+
+class Circle {}
+class Square {}
+const Circle = 'Phutility\Test\Func\Circle';
+const Square = 'Phutility\Test\Func\Square';
+ 
+$multi = F::matchType(
+	Circle, Square, 
+	function($c, $s) {
+		return "MATCHED CIRCLE SQUARE";
+	},
+	Square, 
+	function($s) {
+		return "MATCHED SQUARE";
+	});
+
+test::assert("Match Square", $multi(new Square), "MATCHED SQUARE");
+test::assert("Match Circle Square", $multi(new Circle, new Square), "MATCHED CIRCLE SQUARE");
+
+$fact = F::match(
+	0, function() { return 1;},
+	F::x, function($x) use(&$fact) { return $x * $fact($x - 1);});
+	
+test::assert("factorial works", $fact(5), 120);
